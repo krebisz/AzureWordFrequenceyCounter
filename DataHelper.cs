162c9166c1605746d3856a9c3d53d29453239136
The Master <krebisz@gmail.com>
2022-06-13 01:25:20 +0200

    Cleanup

---
+++ DataHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsApp
+{
+    public static class DataHelper
+    {
+        /// <summary>
+        /// Creates the table to hold the word entities
+        /// </summary>
+        /// <param name="table"></param>
+        public static void CreateCloudTable(CloudTable table)
+        {
+            Task taskCreate = table.CreateIfNotExistsAsync();
+            taskCreate.Wait();
+        }
+
+        /// <summary>
+        /// Insert the word entity (table entity) into the Azure non-relational table
+        /// </summary>
+        /// <param name="table">a cloud table</param>
+        /// <param name="partitionKey">the first identifier for the entity</param>
+        /// <param name="rowKey">the second ifentifier for the entity</param>
+        public static void InsertTableEntity(CloudTable table, string partitionKey, string rowKey) //RESOURCE UTILIZED: https://stackoverflow.com/questions/51730269/how-to-upload-a-single-variable-to-azure-table-storage-c-sharp
+        {
+            // Create a new word entity
+            WordEntity wordEntity = new WordEntity(partitionKey, rowKey);
+            wordEntity.Word = partitionKey;
+            int.TryParse(rowKey, out int frequency);
+            wordEntity.Frequency = frequency;
+
+            // Create the TableOperation object that inserts the word entity
+            TableOperation insertOperation = TableOperation.InsertOrReplace(wordEntity);
+
+            // Execute the insert operation
+            Task taskInsert = table.ExecuteAsync(insertOperation);
+            taskInsert.Wait();
+        }
+
+        /// <summary>
+        /// Selects a table (word) entity from the non-relational Azure table
+        /// </summary>
+        /// <param name="table">a cloud table</param>
+        /// <param name="partitionKey">the first identifier for the entity</param>
+        /// <param name="rowKey">the second ifentifier for the entity</param>
+        /// <returns></returns>
+        public static string SelectTableEntity(CloudTable table, string partitionKey, string rowKey)
+        {
+            TableOperation tableOperation = TableOperation.Retrieve<WordEntity>(partitionKey, rowKey);
+            Task<TableResult> tableResult = table.ExecuteAsync(tableOperation);
+            tableResult.Wait();
+            var tableEntity = tableResult.Result;
+
+            WordEntity wordEntity = new WordEntity();
+            wordEntity.Word = ((WordEntity)tableEntity.Result).Word;
+            wordEntity.Frequency = ((WordEntity)tableEntity.Result).Frequency;
+
+            return ("Word in Azure: " + wordEntity.Word + " occurred " + wordEntity.Frequency.ToString() + " times \r\n");
+        }
+
+    }
+}
--- FrequencyFunction.cs
+++ FrequencyFunction.cs
@@ -43,9 +43,9 @@ namespace AzureFunctionsApp
                 CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
                 CloudTable table = tableClient.GetTableReference(TableReference);
 
-                CreateCloudTable(table);
-                InsertTableEntity(table, partitionKey, rowKey);
-                SelectTableEntity(table, partitionKey, rowKey);
+                DataHelper.CreateCloudTable(table);
+                DataHelper.InsertTableEntity(table, partitionKey, rowKey);
+                resultOutput += DataHelper.SelectTableEntity(table, partitionKey, rowKey);
             }
             catch (Exception ex)
             {
@@ -92,60 +92,6 @@ namespace AzureFunctionsApp
             LetterScores.Add('z', 10);
         }
 
-
-        /// <summary>
-        /// Creates the table to hold the word entities
-        /// </summary>
-        /// <param name="table"></param>
-        public static void CreateCloudTable(CloudTable table)
-        {
-            Task taskCreate = table.CreateIfNotExistsAsync();
-            taskCreate.Wait();
-        }
-
-        /// <summary>
-        /// Insert the word entity (table entity) into the Azure non-relational table
-        /// </summary>
-        /// <param name="table">a cloud table</param>
-        /// <param name="partitionKey">the first identifier for the entity</param>
-        /// <param name="rowKey">the second ifentifier for the entity</param>
-        public static void InsertTableEntity(CloudTable table, string partitionKey, string rowKey) //RESOURCE UTILIZED: https://stackoverflow.com/questions/51730269/how-to-upload-a-single-variable-to-azure-table-storage-c-sharp
-        {
-            // Create a new word entity
-            WordEntity wordEntity = new WordEntity(partitionKey, rowKey);
-            wordEntity.Word = partitionKey;
-            int.TryParse(rowKey, out int frequency);
-            wordEntity.Frequency = frequency;
-
-            // Create the TableOperation object that inserts the word entity
-            TableOperation insertOperation = TableOperation.InsertOrReplace(wordEntity);
-
-            // Execute the insert operation
-            Task taskInsert = table.ExecuteAsync(insertOperation);
-            taskInsert.Wait();
-        }
-
-        /// <summary>
-        /// Selects a table (word) entity from the non-relational Azure table
-        /// </summary>
-        /// <param name="table">a cloud table</param>
-        /// <param name="partitionKey">the first identifier for the entity</param>
-        /// <param name="rowKey">the second ifentifier for the entity</param>
-        /// <returns></returns>
-        public static void SelectTableEntity(CloudTable table, string partitionKey, string rowKey)
-        {
-            TableOperation tableOperation = TableOperation.Retrieve<WordEntity>(partitionKey, rowKey);
-            Task<TableResult> tableResult = table.ExecuteAsync(tableOperation);
-            tableResult.Wait();
-            var tableEntity = tableResult.Result;
-
-            WordEntity wordEntity = new WordEntity();
-            wordEntity.Word = ((WordEntity)tableEntity.Result).Word;
-            wordEntity.Frequency = ((WordEntity)tableEntity.Result).Frequency;
-
-            resultOutput += "Word in Azure: " + wordEntity.Word + " occurred " + wordEntity.Frequency.ToString() + " times \r\n";
-        }
-
         public static string ReturnMostFrequentWord(List<WordEntity> WordEntityList, int wordLength = 0)
         {
             string message = string.Empty;
@@ -166,14 +112,13 @@ namespace AzureFunctionsApp
 
             if (wordLength > 0)
             {
-                //These two variables are here to provided the "highest frequency" word found. It is very basic, but proves the functionality requested as to what is stored within Azure
-                partitionKey = word;
-                rowKey = frequency.ToString();
-
                 message = "Most frequent " + wordLength.ToString() + " word: " + word + " occurred " + frequency.ToString() + " times";
             }
             else
             {
+                //These two variables are here to provided the "highest frequency" word found. It is very basic, but proves the functionality requested as to what is stored within Azure
+                partitionKey = word;
+                rowKey = frequency.ToString();
                 message = "Most frequent word: " + word + " occurred " + frequency.ToString() + " times";
             }
 
